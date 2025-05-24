5238f2aef3db5465403f677a6db4725aba5d2d9b
danieltrejos <tmdaneil@gmail.com>
2025-05-24 09:55:48 -0500

    Fix Cors

--- src/constants/cors.ts
+++ src/constants/cors.ts
@@ -1,15 +1,14 @@
-import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const CORS: CorsOptions = {
-    // En el origen del CORS se Reemplaza 'true' con los orígenes específicos para mayor seguridad,
-    // especialmente en producción. 'true' permite CUALQUIER origen.
-    origin: [
-        'http://localhost:3000', // El frontend Next.js en desarrollo
-        // Añade aquí la URL del frontend en producción cuando se despliegue
-        // 'https://tu-aplicacion-frontend.com'
-    ],
-    methods: ['GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS'],
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true
-
-}
+  // En el origen del CORS se Reemplaza 'true' con los orígenes específicos para mayor seguridad,
+  // especialmente en producción. 'true' permite CUALQUIER origen.
+  origin: [
+    'http://localhost:3000' // El frontend Next.js en desarrollo
+    // Añade aquí la URL del frontend en producción cuando se despliegue
+    // 'https://tu-aplicacion-frontend.com'
+  ],
+  methods: ['GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS'],
+  allowedHeaders: 'Content-Type, Authorization',
+  credentials: true
+};
