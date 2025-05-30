6e8b3e05a4c418b874ea07aa93b9bdb114a7cd9d
danieltrejos <tmdaneil@gmail.com>
2025-05-29 20:34:40 -0500

    Insert a la bd

---
+++ prisma/_Product__202505292031.sql
@@ -0,0 +1,8 @@
+INSERT INTO public."Product" ("name",description,price,image,"createdAt","updatedAt",stock,"type") VALUES
+	 ('Malta','malta',8000.0,NULL,'2025-04-20 19:56:23.724','2025-05-28 14:02:20.64',950,'Lager'),
+	 ('Costeña','Costena 330ml',4000.0,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlBxST3savqc834LXLqzKBYkjHiRMA2vu8jQ&s','2025-04-20 17:30:23','2025-04-20 21:07:31.944',200,'Lager'),
+	 ('Duvel','Cerveza Roja',5500.0,'https://www.clubcolombia.com.co/sites/g/files/seuoyk481/files/2023-05/InternaRoja.png.webp','2025-04-19 20:03:56.947','2025-05-19 21:12:14.925',112,'Lager'),
+	 ('Club colombia ','Cervezas club colombia 330ml ',4000.0,NULL,'2025-04-20 18:45:49.469','2025-05-20 00:52:39.408',47,'Cerveza'),
+	 ('Aguila Light','Cerveza la Aguila Light 330ml',3000.0,'https://jotajotafoods.com/wp-content/uploads/2022/06/CER00037.jpg','2025-04-20 19:34:50.599','2025-05-20 12:51:05.595',25,'Lager'),
+	 ('Heineken','Cerveza lager',4500.0,'heineken.com','2025-05-21 00:36:06.229','2025-05-21 00:36:31.912',220,'Lager'),
+	 ('Aguila','Cerveza colombiana',4000.0,'https://www.clubcolombia.com.co/sites/g/files/seuoyk481/files/2023-05/InternaRoja.png.webp','2025-04-19 20:01:30.853','2025-05-30 00:43:08.833',636,'Lager');
