5e6a27e1d76540fd5dd469e4a49fc0f60b5878a9
danieltrejos <tmdaneil@gmail.com>
2025-06-18 14:41:46 -0500

    Modificacion de CORS para recibir solicituddesde otro destino no disponible aun

--- src/constants/cors.ts
+++ src/constants/cors.ts
@@ -4,8 +4,8 @@ export const CORS: CorsOptions = {
   // En el origen del CORS se Reemplaza 'true' con los orígenes específicos para mayor seguridad,
   // especialmente en producción. 'true' permite CUALQUIER origen.
   origin: [
-    'http://localhost:3000' // El frontend Next.js en desarrollo
-    // Añade aquí la URL del frontend en producción cuando se despliegue
+    'http://localhost:3000', // El frontend Next.js en desarrollo
+    'https://proyecto-adso-frontend.up.railway.app'// Añade aquí la URL del frontend en producción cuando se despliegue
     // 'https://tu-aplicacion-frontend.com'
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
