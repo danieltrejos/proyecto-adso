572c1ff5cc31f663154171c960e6e0f4dff86eb1
danieltrejos <tmdaneil@gmail.com>
2025-06-18 17:17:14 -0500

    Corregir cors

--- src/constants/cors.ts
+++ src/constants/cors.ts
@@ -5,7 +5,10 @@ export const CORS: CorsOptions = {
   // especialmente en producción. 'true' permite CUALQUIER origen.
   origin: [
     'http://localhost:3000', // El frontend Next.js en desarrollo
-    'https://proyect-front-final-production.up.railway.app/' // Añade aquí la URL del frontend en producción cuando se despliegue
+    'https://proyect-front-final-production.up.railway.app', // Añade aquí la URL del frontend en producción cuando se despliegue
+    'https://*.up.railway.app',
+    'https://proyecto-adso-production.up.railway.app'
+
     // 'https://tu-aplicacion-frontend.com'
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
